0ba82c85d7f39dccdb5f87b91eeacb3093dc99f7
cassius <admin@mangopi.xyz>
2025-07-27 08:22:24 +0100

    hello

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ document.addEventListener("keydown", function (e) {
         eval(js);
 	}
 });
+/// eruda.js
+document.addEventListener("keydown", function (e) {
+    if (e.ctrlKey && e.shiftKey && e.code === "KeyE") {
+        if (!window.eruda) {
+            let script = document.createElement("script");
+            script.src = "https://cdn.jsdelivr.net/npm/eruda";
+            script.onload = function () {
+                eruda.init();
+                console.log("Eruda console loaded.");
+            };
+            document.body.appendChild(script);
+        } else {
+            eruda.show();
+        }
+    }
+});
